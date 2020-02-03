9ed56c0c42ae848284e055fb27b491b324e010ca
chakri1998 <35056463+chakri1998@users.noreply.github.com>
2020-02-03 13:12:46 +0530

    ok

--- 04-Terraform/03-Ansible-Deploy/local_exec.tf
+++ 04-Terraform/03-Ansible-Deploy/local_exec.tf
@@ -11,8 +11,8 @@ resource "aws_instance" "backend" {
   version = "~> 2.47"
   ami                    = "ami-04763b3055de4860b"
   instance_type          = "t2.micro"
-  key_name               = "var.key_name"
-  vpc_security_group_ids = ["var.sg-id"]
+  key_name               = "${var.key_name}"
+  vpc_security_group_ids = ["${var.sg-id}"]
 
 }
 
